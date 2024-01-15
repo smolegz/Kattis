285f23474ec8fc1b2b9e82fba601cec39f060486
smolegz <zhuohui.koo@gmail.com>
2024-01-15 23:33:11 +0800

    Updated

---
+++ Solutions/ultraquicksort.java
@@ -0,0 +1,89 @@
+import java.util.*;
+import java.io.*;
+
+public class ultraquicksort {
+    public static class FastScanner {
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+
+    public static long swaps = 0;
+
+    public static void mergeSort(int[] a, int n) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+
+        merge(a, l, r, mid, n - mid);
+    }
+
+    public static void merge(
+            int[] a, int[] l, int[] r, int left, int right) {
+
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                a[k++] = l[i++];
+
+            }
+            else {
+                a[k++] = r[j++];
+                swaps += (left+right) / 2 -i;
+            }
+        }
+        while (i < left) {
+            a[k++] = l[i++];
+
+        }
+        while (j < right) {
+            a[k++] = r[j++];
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        FastScanner sc = new FastScanner();
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0;i<n;i++) arr[i] = sc.nextInt();
+        mergeSort(arr,n);
+        System.out.println(swaps);
+    }
+}
